5acac7bd2d15543d1e095c2c969ceccd507eb9ce
kyle <kconst@gmail.com>
2019-02-11 00:13:20 +0000

    removing history

--- client/src/components/cities/CitiesList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import City from "./City";
-
-const CitiesList = ({ cities }) =>
-  cities.map(city => <City key={city.name} city={city} />);
-
-export default CitiesList;
--- models/cityModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const citySchema = new Schema({
-  name: { type: String },
-  country: { type: String }
-});
-module.exports = City = mongoose.model("City", citySchema);
